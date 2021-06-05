7d5125479cec399c8117ad128ccb577bede57a92
nmparfait <mbamu.developer@gmail.com>
2021-06-05 14:25:48 -0400

    submenu links done

--- app.js
+++ app.js
@@ -36,3 +36,36 @@ sidebar.innerHTML = sublinks
   </article>`;
   })
   .join('');
+
+// links
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener('mouseover', function (e) {
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    const tempPage = sublinks.find((link) => link.page === text);
+    if (tempPage) {
+      const {page, links} = tempPage;
+      submenu.classList.add('show');
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+      submenu.innerHTML = `
+      <section>
+        <h4>${page}</h4>
+        <div class="submenu-center col-2">
+        ${links
+          .map((link) => {
+            return `<a href="${link.url}">
+            <i class="${link.icon}"></i> ${link.label}
+          </a>`;
+          })
+          .join('')}
+        </div>
+      </section>
+      `;
+    }
+  });
+});
